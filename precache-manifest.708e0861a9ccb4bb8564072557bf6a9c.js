8f887914c6165a586b21ac39aae20740ff65d173
Code Sandbox <hello@codesandbox.io>
2022-07-27 11:19:30 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.708e0861a9ccb4bb8564072557bf6a9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "710015bdc850f95a6750",
+    "url": "https://zawiar.github.io/csb-1408qq/static/js/main.710015bd.chunk.js"
+  },
+  {
+    "revision": "35291bb2169bef69952a",
+    "url": "https://zawiar.github.io/csb-1408qq/static/css/1.ee47582e.chunk.css"
+  },
+  {
+    "revision": "35291bb2169bef69952a",
+    "url": "https://zawiar.github.io/csb-1408qq/static/js/1.35291bb2.chunk.js"
+  },
+  {
+    "revision": "a1c7bfe13549c4c521df",
+    "url": "https://zawiar.github.io/csb-1408qq/static/js/runtime~main.a1c7bfe1.js"
+  },
+  {
+    "revision": "08cc2eb7131fa09879a655471c8aeb84",
+    "url": "https://zawiar.github.io/csb-1408qq/index.html"
+  }
+];
